474f6ac792de191d547c4eb4132f695f9a61f336
boriyevv <boriyevdev08@gmail.com>
2024-02-14 21:57:51 +0500

    maybe its end

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 const scrollWrapper = document.querySelector('.scroll-wrapper');
 const scrolll = document.querySelector('.scroll');
+const section1 = document.querySelector('.section1');
+const logo = document.querySelector('.logo');
+const navLink = document.querySelectorAll('.nav-link');
+const root = document.querySelector(':root');
+let styleRoot = window.getComputedStyle(root);
+let propValue = styleRoot.getPropertyValue('--nav-link-color')
+
 
 scrollWrapper.addEventListener('scroll', ()=>{
     console.log(scrollWrapper.scrollTop)
-    const nav = document.querySelector('nav');
-    const header = document.querySelector('header');
+    const nav = document.querySelector('.nav');
     if(scrollWrapper.scrollTop > 0){
         nav.classList.add('scrolled')
-        nav.style.background = 'white'
-        header.style.background = 'white'
         scrollWrapper.style.background = 'white'
+        logo.style.color = '#24CCA8'
+        navLink.forEach(element => {
+            element.style.color = '#24CCA8'
+            root.style.setProperty('--nav-link-color', '#24CCA8')
+
+        });
     }else{
         nav.classList.remove('scrolled')
         nav.style.background = '#24CCA8'
-        header.style.background = '#24CCA8'
+        logo.style.color = '#fff'
+        navLink.forEach(element => {
+            element.style.color = '#fff'   
+            root.style.setProperty('--nav-link-color', 'white')
+        });
         scrollWrapper.style.background = '#24CCA8'
         
     }
